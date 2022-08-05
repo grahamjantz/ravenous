b45d301e651d89dee0341809a84b5fe1d0f00975
grahamjantz <grahammrjantz@gmail.com>
2022-08-04 21:25:36 -0600

    initial commit'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+// import logo from '/Users/grahamjantz/Desktop/EVERYTHING/Programming/ravenous/src/logo.svg';
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from './Business/Business.js';
+
+class BusinessList extends React.Component {
+    render() {(
+        <div className="BusinessList">
+            <Business /> 
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    )}
+}
+
+export default <BusinessList />;
